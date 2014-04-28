88f26f2607dfa5c6332bcfcb7850bd5c2c420362
Adam Parrish <aparrish@neosavvy.com>
2014-04-28 08:47:42 -0400

    Insert.

--- src/main/resources/application/view/content/home-ctrl.js
+++ src/main/resources/application/view/content/home-ctrl.js
@@ -11,18 +11,14 @@ Neosavvy.Controllers.controller('view.content.HomeController',
             $firebase) {
 
             var gear = new Firebase("https://hiker.firebaseio.com/gear");
-            // Automatically syncs everywhere in realtime
             $scope.gear = $firebase(gear);
+
+            $scope.newItem = {}
 
             $scope.addGear = function() {
 
-                $scope.gear.$add({
-                    name: "Pack",
-                    weight: "100grams",
-                    description: "Really awesome pack",
-                    purchaseLink: "http://rei.com/blah",
-                    imageUrl: "http://placehold.it/"
-                });
+                $scope.gear.$add($scope.newItem);
+                $scope.newItem = {}
 
             }
 
